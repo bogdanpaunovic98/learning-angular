b9f71470331ffc5f1323793c07234daa27aff9a3
bogdan.paunovic <bogdan.paunovic@vegaitglobal.com>
2025-09-04 14:56:30 +0200

    Login feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Login',
+    path: 'login',
+    loadComponent: () => import('../features/login/login.component').then((c) => c.Login),
+  },
+];
